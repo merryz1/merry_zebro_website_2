1fb13c3c60f6408421be0475409dc1b5f0b0a2f1
Merry Zebro <merryzebro@gmail.com>
2022-12-31 23:06:43 -0500

    done

--- script.js
+++ script.js
@@ -11,3 +11,6 @@ navLinks.forEach(link => {
     });
   });
 });
+
+
+
--- toggle-animation.js
+++ toggle-animation.js
@@ -1,7 +1,13 @@
 function toggleAnimation() {
-    const header = document.querySelector("header");
-    header.classList.toggle("stop-animation");
+    const h1 = document.querySelector("section#home h1");
+    h1.classList.toggle("stop-animation");  
+    const comp = document.querySelector(".computer" )
+    comp.classList.toggle("stop-animation");  
+
+
   }
+
+
 
 
   function scrollToElement(elementId) {
@@ -9,7 +15,7 @@ function toggleAnimation() {
     const targetElement = document.getElementById(elementId);
   
     // Calculate the distance from the top of the page to the target element
-    const elementOffset = targetElement.offsetTop - 80;
+    const elementOffset = targetElement.offsetTop -80;
   
     // Smoothly scroll to the target element
     window.scroll({
@@ -18,3 +24,67 @@ function toggleAnimation() {
       behavior: 'smooth'
     });
   }
+
+  
+
+
+function scrollToAbout() {
+  
+  // Get the next section
+  var nextSection = document.querySelector('#about');
+
+  // Get the top position of the next section
+  var nextSectionTop = nextSection.offsetTop;
+
+  // Scroll to the top of the next section
+  window.scrollTo({ top: nextSectionTop, behavior: 'smooth' });
+}
+
+function scrollToSkills() {
+  
+  // Get the next section
+  var nextSection = document.querySelector('#skills');
+
+  // Get the top position of the next section
+  var nextSectionTop = nextSection.offsetTop;
+
+  // Scroll to the top of the next section
+  window.scrollTo({ top: nextSectionTop, behavior: 'smooth' });
+}
+
+function scrollToExperience() {
+  
+  // Get the next section
+  var nextSection = document.querySelector('#experience');
+
+  // Get the top position of the next section
+  var nextSectionTop = nextSection.offsetTop - 50;
+
+  // Scroll to the top of the next section
+  window.scrollTo({ top: nextSectionTop, behavior: 'smooth' });
+}
+
+
+
+function scrollToProjects() { // Get the next section
+  var nextSection = document.querySelector('#projects');
+
+  // Get the top position of the next section
+  var nextSectionTop = nextSection.offsetTop;
+
+  // Scroll to the top of the next section
+  window.scrollTo({ top: nextSectionTop, behavior: 'smooth' });
+}
+
+function scrollToContact(){
+   // Get the next section
+   var nextSection = document.querySelector('#contact');
+
+   // Get the top position of the next section
+   var nextSectionTop = nextSection.offsetTop;
+ 
+   // Scroll to the top of the next section
+   window.scrollTo({ top: nextSectionTop, behavior: 'smooth' });
+ }
+
+
